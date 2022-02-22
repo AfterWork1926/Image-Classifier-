441be3270fe73ed9c6331d6996736d299ef51bf2
Roberto <roberto_farella@libero.it>
2022-02-22 15:03:35 +0100

    Initial Commit

---
+++ Chal/ChalApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChalApp.swift
+//  Chal
+//
+//  Created by Roberto Farella on 22/02/22.
+//
+
+import SwiftUI
+
+@main
+struct ChalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
